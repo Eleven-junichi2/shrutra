e8cc9f517541561d8d85c50a39eb562ce56ffde8
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-06-15 21:42:48 +0900

    make func for load files

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use core::panic;
 use std::collections::HashMap;
 use std::default::Default;
 use std::env;
-use std::ffi::OsString;
+use std::error;
+// use std::ffi::OsString;
 use std::fs;
+use std::io::ErrorKind;
 // use std::string::ToString;
 use std::path::PathBuf;
 
@@ -30,70 +32,90 @@ enum Language {
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
     language: Language,
-    path_to_store_recipes: OsString,
+    recipes_path: String,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
             language: Language::Japanese,
-            path_to_store_recipes: "".into(),
+            recipes_path: ".".into(),
         }
     }
 }
 
-fn main() {
-    todo!("implement process for make config file when first execution of app");
-    // --
-    
-    // --
-
-    let exe_path = match env::current_exe() {
-        Ok(exe_path) => {
-            println!("{}", exe_path.display());
-            exe_path
-        }
-        Err(e) => {
-            panic!("failed to get current exe path: {e}");
-        }
-    };
-    let exe_dirpath = exe_path.parent().unwrap();
-
-    // -try to load config file-
-    let mut config_filepathes = [
-        exe_dirpath.join(&CONFIG_FILENAME),
-        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(&CONFIG_FILENAME),
-        env::current_dir().unwrap().join(&CONFIG_FILENAME),
-        env::current_dir().unwrap().join("src").join(&CONFIG_FILENAME),
-    ]
-    .into_iter();
+fn load_file_from_candidate_paths<'a>(
+    filepath_candidates: &mut impl Iterator<Item = &'a PathBuf>,
+) -> Result<String, String> {
     let content = loop {
-        if let Some(path_) = config_filepathes.next() {
+        if let Some(path_) = filepath_candidates.next() {
             match fs::read_to_string(&path_) {
-                Ok(content) => {println!("search {:?}", path_); break content},
+                Ok(content) => break content,
                 Err(_) => {
                     continue;
                 }
             };
         } else {
-            panic!("failed to load config.toml file");
+            return Err("file not found in given candidate paths".to_string());
         };
     };
-    let config: Config = toml::from_str(&content).expect("invalid config file");
+    Ok(content)
+}
+
+fn load_config<'a>(filepath_candidates: &mut impl Iterator<Item = &'a PathBuf>) -> Config {
+    // -try to load config file-
+    // --
+    toml::from_str(&load_file_from_candidate_paths(filepath_candidates).unwrap())
+        .expect("invalid config file")
+}
+
+// fn load_i18ntexts() -> HashMap<String, Value> {
+//     let i18n_filepath = exe_dirpath
+//         .join(I18N_DIRNAME)
+//         .join(config.language.to_string() + ".json");
+//     let content = fs::read_to_string(&i18n_filepath).expect(&format!(
+//         "failed to load localization file (it should be at `{}`)",
+//         &i18n_filepath.display()
+//     ));
+//     let i18ntexts: HashMap<String, Value> =
+//         serde_json::from_str(&content).expect("invalid json file");
+// }
+
+fn main() {
+    let exe_path = env::current_exe().unwrap();
+    let exe_dirpath = exe_path.parent().unwrap();
+
+    let config = load_config(
+        &mut [
+            exe_dirpath.join(&CONFIG_FILENAME),
+            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(&CONFIG_FILENAME),
+            env::current_dir().unwrap().join(&CONFIG_FILENAME),
+            env::current_dir()
+                .unwrap()
+                .join("src")
+                .join(&CONFIG_FILENAME),
+        ]
+        .iter(),
+    );
+
+    // let i18ntexts = load_i18ntexts();
+    // todo!("implement process for make config file when first execution of app");
     // --
 
-    let i18n_filepath = exe_dirpath
-        .join(I18N_DIRNAME)
-        .join(config.language.to_string() + ".json");
-    let content = fs::read_to_string(&i18n_filepath).expect(&format!(
-        "failed to load localization file (it should be at `{}`)",
-        &i18n_filepath.display()
-    ));
-    let i18ntexts: HashMap<String, Value> =
-        serde_json::from_str(&content).expect("invalid json file");
-    println!("-result-");
-    println!("i18ntexts {:?}", i18ntexts);
-    println!("config {:?}", config);
+    // --
+
+    // let i18n_filepath = exe_dirpath
+    //     .join(I18N_DIRNAME)
+    //     .join(config.language.to_string() + ".json");
+    // let content = fs::read_to_string(&i18n_filepath).expect(&format!(
+    //     "failed to load localization file (it should be at `{}`)",
+    //     &i18n_filepath.display()
+    // ));
+    // let i18ntexts: HashMap<String, Value> =
+    //     serde_json::from_str(&content).expect("invalid json file");
+    // println!("-result-");
+    // println!("i18ntexts {:?}", i18ntexts);
+    // println!("config {:?}", config);
     // let name = Text::new("").prompt();
 
     // match name {
