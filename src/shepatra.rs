96ff9a5b365e3721f45677df6dafca40e01478a1
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-06-17 09:22:33 +0900

    add hash funcs

--- src/shepatra.rs
+++ src/shepatra.rs
@@ -2,7 +2,9 @@
 // use base64ct::{Base64, Encoding};
 use digest::DynDigest;
 
+use blake2::{Blake2b512, Blake2s256};
 use sha2::{Sha256, Sha512};
+use sha3::{Sha3_256, Sha3_512};
 use strum_macros::{Display, EnumIter, EnumString};
 
 #[derive(EnumIter, Debug, Display, EnumString)]
@@ -11,6 +13,14 @@ pub enum HashFuncNames {
     SHA256,
     #[strum(serialize = "SHA-512")]
     SHA512,
+    #[strum(serialize = "SHA3-256")]
+    SHA3_256,
+    #[strum(serialize = "SHA3-512")]
+    SHA3_512,
+    #[strum(serialize = "Blake2b512")]
+    Blake2b512,
+    #[strum(serialize = "Blake2s256")]
+    Blake2s256,
 }
 
 #[derive(Debug)]
@@ -24,6 +34,10 @@ pub fn hash_with_recipe(str_to_be_hashed: &String, recipe: &Recipe) -> String {
         let mut hasher: Box<dyn DynDigest> = match hashfunc_name {
             HashFuncNames::SHA256 => Box::new(Sha256::default()),
             HashFuncNames::SHA512 => Box::new(Sha512::default()),
+            HashFuncNames::SHA3_256 => Box::new(Sha3_256::default()),
+            HashFuncNames::SHA3_512 => Box::new(Sha3_512::default()),
+            HashFuncNames::Blake2s256 => Box::new(Blake2s256::default()),
+            HashFuncNames::Blake2b512 => Box::new(Blake2b512::default()),
         };
         hasher.update(&str_to_be_hashed.as_bytes());
         str_to_be_hashed = hasher
@@ -48,5 +62,16 @@ fn test_hash_with_recipe() {
                 layers: vec![HashFuncNames::SHA256],
             },
         )
+    );
+}
+
+#[test]
+fn test_all_hash_func() {
+    use strum::IntoEnumIterator;
+    hash_with_recipe(
+        &"a".to_string(),
+        &Recipe {
+            layers: HashFuncNames::iter().collect(),
+        },
     );
 }
